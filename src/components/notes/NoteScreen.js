30421ca8fca39bb20df9a48e22de486e0881c1c6
Max Alva <alva85@gmail.com>
2021-07-21 09:01:39 -0500

    HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,27 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input
+                    className="notes__title-input"
+                    type="text"
+                    autoComplete="of"
+                    placeholder="Title"
+                />
+
+                <textarea
+                    className="notes__textarea"
+                    placeholder="What happened today"
+                ></textarea>
+
+                <div className="notes__image">
+                    <img
+                        src="https://neilpatel.com/wp-content/uploads/2017/09/image-editing-tools.jpg"
+                        alt=""
+                    />
+                </div>
+            </div>
         </div>
     );
 };
