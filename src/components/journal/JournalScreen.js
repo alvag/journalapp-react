3f6d066ff997fb3aa8bcb94ae551da89c490f7ef
Max Alva <alva85@gmail.com>
2021-07-21 23:01:25 -0500

    Configurando redux

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
-import { NothingSelected } from './NothingSelected';
 import { NoteScreen } from '../notes/NoteScreen';
 
 export const JournalScreen = () => {
     return (
-        <div className="journal__main-content">
+        <div className='journal__main-content'>
             <Sidebar />
 
             <main>
--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -3,19 +3,19 @@ import { JournalEntries } from './JournalEntries';
 
 export const Sidebar = () => {
     return (
-        <div className="journal__sidebar">
-            <div className="journal__sidebar-navbar mt-5">
+        <div className='journal__sidebar'>
+            <div className='journal__sidebar-navbar mt-5'>
                 <h3>
-                    <i className="far fa-moon"></i>
+                    <i className='far fa-moon' />
                     <span> Max</span>
                 </h3>
 
-                <button className="btn">Logout</button>
+                <button className='btn'>Logout</button>
             </div>
 
-            <div className="journal__new-entry">
-                <i className="far fa-calendar-plus fa-5x"></i>
-                <p className="mt-5">New entry</p>
+            <div className='journal__new-entry'>
+                <i className='far fa-calendar-plus fa-5x' />
+                <p className='mt-5'>New entry</p>
             </div>
 
             <JournalEntries />
